533bfbcf76ce13bff13c800c4baae74dd0667300
Alon Doron <alon.d@taboola.com>
2017-01-11 22:04:04 +0200

    holy hell

--- plugin/src/js/popup.js
+++ plugin/src/js/popup.js
@@ -2,7 +2,7 @@ function renderRecs(recs) {
     var trHTML = '<tr><th colspan=2>You May Like</th></tr>';
 
     $.each(recs.items, function (i, item) {
-        trHTML += '<tr><td><img id="thmbnl" src="' + item[1] + '"/></td><td valign="middle">' + item[0] + '</td></tr>';
+        trHTML += '<tr><td><img id="thmbnl" src="' + item[1] + '"/></td><td valign="middle"><a href="' + item[0] + '" onclick="chrome.tabs.create({url:this.href})">' + item[0] + '</a></td></tr>';
     });
 
     $('#recList').append(trHTML);
@@ -15,5 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .fail(function() {
         $('#recList').append("Couldn't get recommendations");
+    }).done(function () {
+        // var hrefs = document.getElementsByTagName("a");
+        //
+        // function openLink() {
+        //     chrome.tabs.create({active: true, url: this.href});
+        //     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        //         // var tab = tabs[0];
+        //         // chrome.tabs.update(tab.id, {url: "www." + tab.url.substring(tab.url.lastIndexOf("/")+1)});
+        //
+        //     // });
+        // }
+        //
+        // for (var i=0,a; a=hrefs[i]; ++i) {
+        //     hrefs[i].addEventListener('click', openLink);
+        // }
+        // $('body').on('click', 'a', function(){
+        //     chrome.tabs.create({url: $(this).attr('href')});
+        //     return false;
+        // });
     });
+});
+window.addEventListener('click',function(e){
+    if(e.target.href!==undefined){
+        chrome.tabs.create({url:e.target.href})
+    }
 });
--- plugin/src/js/utilities.js
+++ plugin/src/js/utilities.js
@@ -2,16 +2,16 @@ USER_ID = 0;
 /**
  * Created by idan.a on 11/01/2017.
  */
-$("p").hover(function () {
-    $(this).css("background-color", "yellow");
-}, function () {
-    $(this).css("background-color", "pink");
-});
-$(document).ready(function () {
-    $("#sub1").mouseover(function () {
-        $("#welcome").toggle();
-    });
-});
+// $("p").hover(function () {
+//     $(this).css("background-color", "yellow");
+// }, function () {
+//     $(this).css("background-color", "pink");
+// });
+// $(document).ready(function () {
+//     $("#sub1").mouseover(function () {
+//         $("#welcome").toggle();
+//     });
+// });
 
 function createCookie(name, value, expires, path, domain) {
     var cookie = name + "=" + escape(value) + ";";
