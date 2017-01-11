d2c023bda32d6ab2511923a8d13764e4b0647fa9
Ran Harari <ran.h@taboola.com>
2017-01-11 15:19:49 +0200

    refactoring

--- disco-server/hist/index.js
+++ disco-server/hist/index.js
@@ -1,16 +1,11 @@
 var logger = require('../logger').logger
 var codes = require('../codes')
 
-function getItems() {
-    return {items: ['item 1', 'item 2', 'item 3']}
-}
-
 function route(server) {
-    server.get('/disco/get', function (req, res) {
+    server.post('/hist/save', function (req, res) {
         logger.debugReq(req)
-
         res.status(codes.OK)
-            .json(getItems)
+            .json(req.body.params)
     })
 }
 
--- disco-server/index.js
+++ disco-server/index.js
@@ -4,23 +4,17 @@ var logger = require('./logger').logger
 var codes = require('./codes')
 var server = require('express')()
 require('./handlers').add(server)
+var disco = require('./disco')
+var hist = require('./hist')
+
+disco.route(server)
+hist.route(server)
 
 server.get('/', function (req, res) {
     res.send('Aces!');
 })
 
-server.get('/disco/get', function (req, res) {
-    logger.debugReq(req)
 
-    res.status(codes.OK)
-        .json({items: ['item 1', 'item 2', 'item 3']})
-})
-
-server.post('/hist/save', function (req, res) {
-    logger.debugReq(req)
-    res.status(codes.OK)
-        .json(req.body.params)
-})
 
 server.listen(PORT, function () {
     logger.info('Running on ' + PORT)
