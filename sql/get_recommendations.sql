5fdd5e7fee350b481b101b19d5506e370cb9263d
Alexey Pechorin <Alexey.p@taboola.com>
2017-01-11 18:32:32 +0200

    Continue draft of recommendation query

--- sql/get_recommendations.sql
+++ sql/get_recommendations.sql
@@ -1,10 +1,10 @@
 SET @user_id='123';
+
 SELECT
     *
 FROM
-    recommendations
-WHERE
-    category IN
+    recommendations r
+INNER JOIN
     (
         SELECT
             category,
@@ -16,5 +16,15 @@ WHERE
         GROUP BY
             category
         ORDER BY
-            num_views DESC limit 2;
-) ;
+            num_views DESC limit 2 ) user_top_categories
+INNER JOIN
+    (
+        SELECT
+            url
+        FROM
+            user_history_categories
+        WHERE
+            user_id = @user_id ) user_url_history
+ON
+    r.category = user_top_categories.category
+AND user_url_history.url = r.url
